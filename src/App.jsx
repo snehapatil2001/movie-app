139f2863e9c32e3e5d334e3f4cbb7f033e5614a0
Sneha <snehapatildeepali@gmail.com>
2025-02-04 17:40:03 +0530

    chore: fetching data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Search from './components/Search.jsx'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${API_KEY}`
+	}
+}
+
 const App = () => {
-	const [searchTerm, setSearchTerm] = React.useState('');
+	const [searchTerm, setSearchTerm] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+	const [movieList, setMovieList] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const fetchMovies = async () => {
+		setIsLoading(true);
+		setErrorMessage('');
+
+		try {
+			const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+			const response = await fetch(endpoint, API_OPTIONS);
+
+			if (!response.ok) {
+				setErrorMessage('Failed to fetch movies');
+				return;
+			}
+
+			const data = await response.json();
+
+			if (data.results.length === 0) {
+				setErrorMessage('No movies found');
+				setMovieList([]);
+				return;
+			}
+
+			setMovieList(data.results || []);
+		} catch (error) {
+			console.error(`Error fetching movies: ${error}`);
+			setErrorMessage('Error fetching movies. Please try again later.');
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchMovies();
+	}, []);
+
 	return (
 		<main>
 			<div className="pattern" />
@@ -10,8 +59,27 @@ const App = () => {
 				<header>
 					<img src='./hero.png' alt='hero' />
 					<h1>Find <span className='text-gradient'>Movies</span>You'll enjoy without the hassle.</h1>
+					<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 				</header>
-				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+				<section className="all-movies">
+					<h2>All Movies</h2>
+					{isLoading ? (
+						<p className="text-white">Loading...</p>
+					) : errorMessage ? (
+						<p className="text-red-500">{errorMessage}</p>
+					) : (
+						<ul>
+							{movieList.map((movie) => (
+								<li key={movie.id}>
+									<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+									<h3>{movie.title}</h3>
+									<p>{movie.overview}</p>
+								</li>
+							))}
+						</ul>
+					)
+					}
+				</section>
 			</div>
 		</main>
 	)
