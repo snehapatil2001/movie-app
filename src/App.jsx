ca2bc0617502fef3ef3d5acaf1e1edfc265af1a9
Sneha <snehapatildeepali@gmail.com>
2025-02-04 12:31:31 +0530

    chore: search basic structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
-  return (
-	<main>
-		<div className="pattern" />
-
-		<div className="wrapper">
-			<header>
-				<h1>Find <span>Movies</span>You'll enjoy without the hassle.</h1>
-			</header>
-			<p>Search</p>
-		</div>
-	</main>
-  )
+	const [searchTerm, setSearchTerm] = React.useState('');
+	return (
+		<main>
+			<div className="pattern" />
+			<div className="wrapper">
+				<header>
+					<img src='./hero.png' alt='hero' />
+					<h1>Find <span className='text-gradient'>Movies</span>You'll enjoy without the hassle.</h1>
+				</header>
+				<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			</div>
+		</main>
+	)
 }
 
 export default App
