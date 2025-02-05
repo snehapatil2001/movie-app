2102ce9cb46beda50913c2f371f81a016e40fc56
Sneha <snehapatildeepali@gmail.com>
2025-02-05 09:51:25 +0530

    chore: add spinner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -62,9 +63,9 @@ const App = () => {
 					<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 				</header>
 				<section className="all-movies">
-					<h2>All Movies</h2>
+					<h2 className="mt-[40px]">All Movies</h2>
 					{isLoading ? (
-						<p className="text-white">Loading...</p>
+						<Spinner />
 					) : errorMessage ? (
 						<p className="text-red-500">{errorMessage}</p>
 					) : (
